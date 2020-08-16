fb11dce332499a38e94bedab6fad8796a84c6bdd
vladi4560 <64600121+vladi4560@users.noreply.github.com>
2020-08-16 19:15:43 +0300

    Update LandingFlight.java

--- Airport/src/LandingFlight.java
+++ Airport/src/LandingFlight.java
@@ -2,22 +2,28 @@ import java.time.LocalTime;
 import java.time.LocalDateTime;
 
 public class LandingFlight extends Flight implements Comparable<LandingFlight>{
-private String LandedFrom;
+private String LandedFromCity;
+private String LandedFromCountry;
 
 
-
-public LandingFlight(String companyFlight, String numFlight, LocalDateTime scheduledTime, int terminal, String LandedFrom) {
-	super(companyFlight, numFlight, scheduledTime, terminal);
-	this.LandedFrom=LandedFrom;
+public LandingFlight(String companyFlight, String numFlight, LocalDateTime scheduledTime, int terminal, String LandedFrom,String landedFromCountry,String day) {
+	super(companyFlight, numFlight, scheduledTime, terminal,day);
+	this.LandedFromCity=LandedFrom;
+	this.LandedFromCountry=landedFromCountry;
 }
 	
 public String toString() {
-	return super.toString()+"Landed From="+LandedFrom+"\n";
+	return super.toString()+"Landed From City:"+LandedFromCity+"\nLanden from Country:"+LandedFromCountry;
 }
 
 
 public int compareTo(LandingFlight o) {
 	return this.scheduledTime.compareTo(o.scheduledTime);
 }
-	
+public String getLandedFromCity() {
+	return LandedFromCity;
+}
+public String landedFromCountry() {
+	return LandedFromCountry;
+}
 }
