da583c89a6bfb04c4039322e7440e612ee7c76e2
Dorsmandorin <66877313+Dorsmandorin@users.noreply.github.com>
2020-08-18 15:08:08 +0300

    Update DeparturesFlight.java

--- Airport/src/DeparturesFlight.java
+++ Airport/src/DeparturesFlight.java
@@ -5,11 +5,15 @@ import java.time.LocalDateTime;
 public class DeparturesFlight extends Flight implements Comparable<DeparturesFlight> {
 	private String destinationCity;
 	private String destinationCountry;
+	private String DepartureAirport;
 
-	public DeparturesFlight(String companyFlight, String numFlight, LocalDateTime scheduledTime, int terminal, String destinationCity,String destinationCountry,String day) {
-		super(companyFlight, numFlight, scheduledTime, terminal,day);
+	public DeparturesFlight(String companyFlight, String numFlight, LocalDateTime scheduledTime, int terminal,
+			String destinationCity,String destinationCountry,String day, String DepartureAirport) {
+		super(companyFlight, numFlight, scheduledTime, terminal);
 		this.destinationCity = destinationCity;
 		this.destinationCountry= destinationCountry;
+		this.day=day;
+		this.DepartureAirport=DepartureAirport;
 	}
 
 	public int compareTo(DeparturesFlight o) {
@@ -17,7 +21,8 @@ public class DeparturesFlight extends Flight implements Comparable<DeparturesFli
 	}
 	
 	public String toString() {
-		return super.toString() + "Destination Country:" + destinationCountry + "\nDestination City: "+destinationCity;
+		return super.toString() + "Destination Country=" + destinationCountry + "\nDestination City="+destinationCity +"\nDay=" + day +
+	"\nAirport="+DepartureAirport + "\n";
 
 	}
 	public String  getDestinationCity() {
@@ -25,5 +30,9 @@ public class DeparturesFlight extends Flight implements Comparable<DeparturesFli
 	}
 	public String  getDestinationCountry() {
 		return destinationCountry ;
-}
+	}
+	
+	public String getDepartureDay() {
+		return day;
+	}
 }
