351804fb12dc5131487a8b505f5b187435e02af6
Dorsmandorin <66877313+Dorsmandorin@users.noreply.github.com>
2020-08-18 15:08:58 +0300

    Update LandingFlight.java

--- Airport/src/LandingFlight.java
+++ Airport/src/LandingFlight.java
@@ -1,29 +1,38 @@
 import java.time.LocalTime;
 import java.time.LocalDateTime;
 
-public class LandingFlight extends Flight implements Comparable<LandingFlight>{
-private String LandedFromCity;
-private String LandedFromCountry;
+public class LandingFlight extends Flight implements Comparable<LandingFlight> {
+	private String LandedFromCity;
+	private String LandedFromCountry;
+	private String LandingAirport;
 
+	public LandingFlight(String companyFlight, String numFlight, LocalDateTime scheduledTime, int terminal,
+			String LandedFrom, String landedFromCountry, String day, String LandingAirport) {
+		super(companyFlight, numFlight, scheduledTime, terminal);
+		this.LandedFromCity = LandedFrom;
+		this.LandedFromCountry = landedFromCountry;
+		this.day=day;
+		this.LandingAirport=LandingAirport;
+	}
 
-public LandingFlight(String companyFlight, String numFlight, LocalDateTime scheduledTime, int terminal, String LandedFrom,String landedFromCountry,String day) {
-	super(companyFlight, numFlight, scheduledTime, terminal,day);
-	this.LandedFromCity=LandedFrom;
-	this.LandedFromCountry=landedFromCountry;
-}
+	public String toString() {
+		return super.toString() + "Landed From City=" + LandedFromCity + "\nLanden from Country=" + LandedFromCountry +"\nDay=" + day +
+	"\nAirport="+ LandingAirport +"\n";
+	}
+
+	public int compareTo(LandingFlight o) {
+		return this.scheduledTime.compareTo(o.scheduledTime);
+	}
+
+	public String getLandedFromCity() {
+		return LandedFromCity;
+	}
+
+	public String landedFromCountry() {
+		return LandedFromCountry;
+	}
 	
-public String toString() {
-	return super.toString()+"Landed From City:"+LandedFromCity+"\nLanden from Country:"+LandedFromCountry;
-}
-
-
-public int compareTo(LandingFlight o) {
-	return this.scheduledTime.compareTo(o.scheduledTime);
-}
-public String getLandedFromCity() {
-	return LandedFromCity;
-}
-public String landedFromCountry() {
-	return LandedFromCountry;
-}
+	public String getLandingDay() {
+		return day;
+	}
 }
